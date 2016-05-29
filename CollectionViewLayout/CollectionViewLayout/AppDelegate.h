8f3c4692f025e3d8af46f1b67b9fa2e552d10cf2
baozitou <15090071272@163.com>
2016-05-29 20:58:52 +0800

    创建项目

---
+++ CollectionViewLayout/CollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewLayout
+//
+//  Created by 杜习营 on 16/5/29.
+//  Copyright © 2016年 dxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewLayout/CollectionViewLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewLayout
+//
+//  Created by 杜习营 on 16/5/29.
+//  Copyright © 2016年 dxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
